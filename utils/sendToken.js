14a8f71d5b86a0aba40867274ddc5b3271f90a04
Jagadeesh <jagadeesh961982@gmail.com>
2024-08-25 18:15:42 +0530

    token

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -6,6 +6,7 @@ const sendToken=(user,statusCode,res)=>{
         expires:new Date(Date.now()+process.env.COOKIE_EXPIRES_TIME*24*60*60*1000),
         httpOnly:true,
         sameSite:"none",
+        secure:true
     }
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
